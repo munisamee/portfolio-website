8b689312d8784fb20e2b94f108a629ba608c49b4
munisamee <munisa.ermatova@gmail.com>
2021-07-24 00:29:39 +0200

    alert and variables added
    alert and variables (favoriteFood, myName and simpleAddition) are added for practice to simple-js-app

---
+++ simple-js-app/js:/scripts.js
@@ -0,0 +1,21 @@
+alert('Hello world');
+
+let favoriteFood = 'Mexican';
+document.write(favoriteFood);
+
+let myName = 'Bob';
+document.write(myName);
+myName = 'John Doe';
+document.write(myName);
+
+let simpleAddition = 2 + 2;
+document.write(simpleAddition);
+
+let size = 100;
+let doubleSize = size * 2;
+document.write(doubleSize);
+
+let size = 100;
+let doubleSize = size * 2;
+let minSize = (doubleSize * 2) - (size / 2);
+document.write(minSize);
